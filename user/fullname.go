2a62920df8cc82b06663ca77770ce634cf67c4c5
shu.yamaga <s.yamaga.0318@gmail.com>
2025-04-27 08:43:05 +0000

    [update]implement FullName

---
+++ user/fullname.go
@@ -0,0 +1,18 @@
+package user
+
+type FullName struct {
+	firstName string
+	lastName  string
+}
+
+func NewFullName(firstName, lastName string) *FullName {
+	return &FullName{
+		firstName: firstName,
+		lastName:  lastName,
+	}
+}
+
+// Equals compares FullName fields
+func (fn FullName) Equals(other *FullName) bool {
+	return fn.firstName == other.firstName && fn.lastName == other.lastName
+}
