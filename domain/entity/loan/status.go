130b1c08551f6ff9c458be7bbe07412db88aa1db
shu.yamaga <s.yamaga.0318@gmail.com>
2025-05-05 05:22:30 +0000

    [update] add LoanStatus.String method

--- domain/entity/loan/status.go
+++ domain/entity/loan/status.go
@@ -6,5 +6,18 @@ type LoanStatus int
 const (
 	onLoan   LoanStatus = iota // 貸出中
 	returned                   // 返却済み
-	overdue                    // 返却期限超過
+	overdue                    // 延滞
 )
+
+func (ls LoanStatus) String() string {
+	switch ls {
+	case onLoan:
+		return "貸出中"
+	case returned:
+		return "返却済み"
+	case overdue:
+		return "延滞"
+	default:
+		return "予期しない貸出状態"
+	}
+}
